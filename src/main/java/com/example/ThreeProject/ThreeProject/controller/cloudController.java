8450b27851837cd54d3af96b1abf646e9c35db9a
saket kumar keshri <keshrisaket414@gmail.com>
2024-03-26 16:07:56 +0530

    first commit

--- src/main/java/com/example/ThreeProject/ThreeProject/controller/cloudController.java
+++ src/main/java/com/example/ThreeProject/ThreeProject/controller/cloudController.java
@@ -14,6 +14,8 @@ public class cloudController  {
 
     cloudService cloudService;
 
+    cloudModal cloudModal;
+
 
     public cloudController(cloudService cloudService){
         this.cloudService=cloudService;
